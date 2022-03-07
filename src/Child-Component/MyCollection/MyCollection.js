aaa50f810cce096b00a0fc8ff526ffc8d4ecaf96
ArbitechSolution <arbitechsolutions@gmail.com>
2022-03-07 12:39:22 +0500

    Added safeTranferFrom Function

--- src/Child-Component/MyCollection/MyCollection.js
+++ src/Child-Component/MyCollection/MyCollection.js
@@ -1,123 +1,168 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import "./MyCollection.css"
 import Web3 from 'web3';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import Modal from 'react-bootstrap/Modal'
 
 import { nftContratAddress, nftContractAbi } from "../../Component/Utils/Nft"
 import axios from 'axios';
+import { toast } from 'react-toastify';
 function MyCollection() {
     let max = 200;
-    let [btnTxt, setBtTxt] = useState("Connect")
-    let [imageArray,setImageArray] = useState([]);
-    // let [collection, setCollection] = useState([])
+    let [btnTxt, setBtTxt] = useState("Connect");
+    let [modalShow, setModalShow] = useState(false)
+    let [clickedIndexes, setClickedIndexes] = useState()
+    let [imageArray, setImageArray] = useState([]);
     let [test, setTest] = useState([])
     let [initialLimit, setInitiaLimit] = useState(0);
-    let [finalLimit, setFinalLimit]=useState(6)
+    let [finalLimit, setFinalLimit] = useState(6)
     let [num, setnum] = useState(0);
-    let {acc} = useSelector(state =>state.connectWallet)
+    let { acc } = useSelector(state => state.connectWallet)
+    let toAddress =useRef("")
+    const allImagesNfts = async () => {
+        if (acc == "No Wallet") {
+            console.log("wallet");
+            setBtTxt("Connect Wallet")
+        }
+        else if (acc == "Wrong Network") {
+            setBtTxt("Wrong Network")
+        } else if (acc == "Connect Wallet") {
+            console.log("Connect Wallet");
+        }
 
-    const allImagesNfts=async()=>{
-            // console.log("ACC=",acc)
-            if (acc == "No Wallet") {
-                console.log("wallet");
-                setBtTxt("Connect Wallet")
+        else {
+            console.log("Inside")
+            const web3 = window.web3;
+            let nftContractOf = new web3.eth.Contract(nftContractAbi, nftContratAddress);
+            let simplleArray = [];
+            let walletOfOwner = await nftContractOf.methods.walletOfOwner(acc).call()
+            let walletLength = walletOfOwner.length
+            console.log("walletOfOwner", walletLength);
+            if (parseInt(walletLength) > 0) {
+                for (let i = 0; i < parseInt(walletLength); i++) {
+                    try {
+                        let res = await axios.get(`https://gateway.pinata.cloud/ipfs/QmPQxoBcxfkDc28mDSxXABkC74HTimND6ESNhubqrNnuGz/${walletOfOwner[i]}.json`)
+                        //   console.log("Indexes", i);
+                        let imageUrl = res.data.image;
+                        simplleArray.push(imageUrl);
+                        setImageArray(simplleArray)
+
+                        console.log("Getting Response", res.data.image);
+                    } catch (e) {
+                        console.log("Error while Fetching Api", e)
+                    }
+                }
             }
-            else if (acc == "Wrong Network") {
-                setBtTxt("Wrong Network")
-            }else if (acc=="Connect Wallet"){
-                console.log("Connect Wallet");
-            } 
-            
-            else {
-                console.log("Inside")
-                            const web3 = window.web3;
-                            let nftContractOf = new web3.eth.Contract(nftContractAbi, nftContratAddress);
-                            let simplleArray =[];
-                            let walletOfOwner = await nftContractOf.methods.walletOfOwner(acc).call()
-                            let walletLength = walletOfOwner.length
-                            console.log("walletOfOwner", walletLength);
-                            if(parseInt(walletLength)>0){
-                                for( let i=0; i<=parseInt(walletLength); i++){
-                                    try{
-                                      let res = await axios.get(`https://gateway.pinata.cloud/ipfs/QmPQxoBcxfkDc28mDSxXABkC74HTimND6ESNhubqrNnuGz/${walletOfOwner[i]}.json`)
-                                    //   console.log("Indexes", i);
-                                    let imageUrl = res.data.image;
-                                    simplleArray.push(imageUrl);
-                                    setImageArray(simplleArray)
-        
-                                      console.log("Getting Response", res.data.image);
-                                    }catch(e){
-                                        console.log("Error while Fetching Api",e)
-                                    }
-                                      
-                                }
-                            }
-                       
-                       
-
-            }
-
+        }
     }
-
-    // const mycollection = async () => {
-    //     let acc = await loadWeb3();
-    //     // console.log("ACC=",acc)
-    //     if (acc == "No Wallet") {
-    //         console.log("wallet");
-    //         setBtTxt("Connect Wallet")
-    //     }
-    //     else if (acc == "Wrong Network") {
-    //         setBtTxt("Wrong Network")
-    //     } else {
-    //         let myAcc = acc?.substring(0, 4) + "..." + acc?.substring(acc?.length - 4);
-    //         setBtTxt(myAcc);
-
-
-    //         const web3 = window.web3;
-    //         let nftContractOf = new web3.eth.Contract(nftContractAbi, nftContratAddress);
-    //         let walletOfOwner = await nftContractOf.methods.walletOfOwner(acc).call()
-    //         let walletLength = walletOfOwner.length
-    //         console.log("walletOfOwner", walletLength);
-    //         let resArray = []
-    //         let dummyAray = [...test];
-    //         dummyAray.map((item) => {
-    //             resArray.push(item)
-    //         })
-
-
-    //         for (let i = 1; i < walletLength; i++) {
-
-    //             let passVariable = walletOfOwner[i];
-
-
-    //             await axios.get(`https://gateway.pinata.cloud/ipfs/QmP3CU9tcQGYbBYzzhWk8tc4fcQePHXKwJqYBMY3LZNBw7/${passVariable}.json`).then((res) => {
-    //                 resArray.push(res);
-    //                 console.log("res", res.data);
-    //             })
-
-    //         }
-    //         setTest(resArray);
-    //     }
-    // }
-
     const ClickNext = () => {
         if (finalLimit < 12 && initialLimit >= 0) {
-            setInitiaLimit(initialLimit+6)
-            setFinalLimit(finalLimit+6)
+            setInitiaLimit(initialLimit + 6)
+            setFinalLimit(finalLimit + 6)
         }
     }
     const ClickPrevious = () => {
         if (initialLimit <= max && finalLimit > 6) {
-            setInitiaLimit(initialLimit-6)
-            setFinalLimit(finalLimit-6)
+            setInitiaLimit(initialLimit - 6)
+            setFinalLimit(finalLimit - 6)
 
         }
     }
+
+    const clickedImage = (index) => {
+        setClickedIndexes(index)
+        setModalShow(true)
+        console.log("Clicked Image", index);
+    }
+    const transferNft =async()=>{
+        if (acc == "No Wallet") {
+            console.log("wallet");
+            setBtTxt("Connect Wallet")
+        }
+        else if (acc == "Wrong Network") {
+            setBtTxt("Wrong Network")
+        } else if (acc == "Connect Wallet") {
+            console.log("Connect Wallet");
+        }else{
+            let userEnteredAddress =toAddress.current.value;
+            const web3=window.web3;
+            let stringLength=userEnteredAddress.length;
+            if (parseInt(stringLength)==42){
+                try{
+                    let nftContractOf = new web3.eth.Contract(nftContractAbi, nftContratAddress);
+                    let walletOfOwner = await nftContractOf.methods.walletOfOwner(acc).call()
+                    let tokenId =walletOfOwner[clickedIndexes];
+                    await nftContractOf.methods.safeTransferFrom(acc,userEnteredAddress,tokenId).send({
+                        from:acc
+                    })    
+                    toast.success("Transaction confirmed")
+                    setModalShow(false)
+                }catch(e){
+                    toast.error("Transaction Failed")
+                    console.log("Error While Transfering Nft",e);
+
+                }
+
+            }else{
+                toast.error("Invalid Address")
+            }
+
+        }
+    }
+
     useEffect(() => {
-            allImagesNfts() 
+        allImagesNfts()
     }, [])
     return (
+
+
+
+
         <div className='StakePageImagess pb-5'>
+            {
+                modalShow?
+                <Modal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                // {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">
+                   Confirm
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className='d-flex justify-content-center align-items-center flex-column'>
+                  <h4>Selected Image</h4>
+                  {/* {imageArray.map((items, index) => { */}
+                                {/* return ( */}
+                                    <div className='col-lg-3 col-md-5 mycollectionsModal p-2 m-2'>
+                                        <img src={imageArray[clickedIndexes]} className='myCollectionsImage ' />
+                                        {/* <span className='imageText text-white'  >&nbsp;&nbsp;{items.data.dna}</span> */}
+                                        <div>
+                                            {/* <span className='imageText text-white'  >&nbsp;&nbsp;1</span> */}
+                                            <p className='collectionsTextSmall m-2  '>Common</p>
+                                        </div>
+
+                                        <div className="d-grid gap-2">
+                                        <p className='collectionsTextSmall mt-2  '>To:</p>
+                                            <input ref={toAddress}/>
+                                            <button onClick={()=>transferNft()} className='btn btnStakePage' size="lg">
+                                                Confirm
+                                            </button>
+                                     </div>
+                                    </div>
+                                {/* ) */}
+                            {/* })} */}
+                </Modal.Body>
+                <Modal.Footer>
+                  {/* <Button onClick={props.onHide}>Close</Button> */}
+                </Modal.Footer>
+              </Modal>:<></>
+            }
+
             <div className='container pt-3'>
                 <div className='row d-flex justify-content-center align-items-center pb-3'>
                     <div className='col-md-12 col-11 pt-4 pb-4'>
@@ -126,35 +171,31 @@ function MyCollection() {
                                 <p className='stakepageP'>My NFT Collection</p>
                             </div>
                             <div className='col-md-3 d-flex justify-content-end'>
-                                <button className='btn btnstake'>{acc ==="No Wallet" ? "Insatll metamask" :acc ==="Connect Wallet" ? acc  : acc ==="Connect to Rinkebey"? acc :acc.substring(0,5) + "..." + acc.substring(acc.length - 5)  }</button>
+                                <button className='btn btnstake'>{acc === "No Wallet" ? "Insatll metamask" : acc === "Connect Wallet" ? acc : acc === "Connect to Rinkebey" ? acc : acc.substring(0, 5) + "..." + acc.substring(acc.length - 5)}</button>
                             </div>
                         </div>
 
                         <div className='row d-flex justify-content-center mt-3'>
-                            {imageArray.slice(initialLimit, finalLimit).map((items,index) => {
+                            {imageArray.slice(initialLimit, finalLimit).map((items, index) => {
                                 return (
                                     <div className='col-lg-3 col-md-5 mycollections p-2 m-2'>
                                         <img src={imageArray[index]} className='myCollectionsImage ' />
                                         {/* <span className='imageText text-white'  >&nbsp;&nbsp;{items.data.dna}</span> */}
                                         <div>
+                                            <span className='imageText text-white'  >&nbsp;&nbsp;{index}</span>
                                             <p className='collectionsText mt-3'>#20211 Tiger Master</p>
                                             <p className='collectionsTextSmall'>Common</p>
                                         </div>
 
                                         <div className="d-grid gap-2">
-                                            <button className='btn btnStakePage' size="lg">
+                                            <button onClick={() => clickedImage(index)} className='btn btnStakePage' size="lg">
                                                 Transfer
                                             </button>
-                                        </div>
-
+                                     </div>
                                     </div>
                                 )
-
-
-
                             })}
                         </div>
-
                     </div>
                     <div className='row d-flex flex-row justify-content-center justify-content-evenly' >
 
